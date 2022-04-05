0d22160d1a2d9d22aa98da349870fd22a80d3382
NikitaBorovkov <99392505+NikitaBorovkov@users.noreply.github.com>
2022-04-05 23:00:07 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,6 +4,27 @@
 
 template<typename T, int size>
 class TStack {
-
+ private:
+     T arr[100];
+     int t;
+ public:
+     TStack() : t(-1) {}
+     bool isEmpty() const {
+         return t == -1;
+     }
+     bool isFull() const {
+         return t == size - 1;
+     }
+     T get() {
+         return arr[top];
+     }
+     void pop() {
+         if (t >= 0)
+             t--;
+     }
+     void push(T value) {
+         if (t < size - 1)
+             arr[++] = value;
+     }
 };
 #endif  // INCLUDE_TSTACK_H_
